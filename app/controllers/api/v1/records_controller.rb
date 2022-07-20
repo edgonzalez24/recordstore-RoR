87cb5590c342012f20c4c5a02cb28211e8b6abdd
Edwin <edgoca2498@gmail.com>
2022-07-20 08:39:36 -0600

    chore: Client Vue.js setup

--- app/controllers/api/v1/records_controller.rb
+++ app/controllers/api/v1/records_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class RecordsController < ApplicationController
       before_action :authorize_access_request!
-      before_action :set_record, only: %i[ show update destroy ]
+      before_action :set_record, only: [:show, :update, :destroy]
 
       # GET /records
       def index
---
+++ client/src/pages/Signin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="max-w-sm m-auto my-8">
+    <div class="border p-10 border-gray-500 shadow rounded italic">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia officiis sint dignissimos delectus quae magnam unde, veritatis ipsam est! Nostrum beatae mollitia minima aliquam consectetur ipsum exercitationem, ipsa quae iste?
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
